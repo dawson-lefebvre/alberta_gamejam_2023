fb53fc68dec14d9e9b5b17ae01546fbad6636033
eHolmesSchool <squippysquerp@gmail.com>
2023-08-19 14:34:03 -0600

    sfx good

---
+++ Assets/Scripts/evantest/SFXCollide.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SFXCollide : MonoBehaviour
+{
+    [SerializeField] AudioClip clip;
+    [SerializeField] AudioSource source;
+
+    [SerializeField] int delayFramesDefault = 30;
+    int delayFrames;
+
+    private void Awake()
+    {
+        //source.playOnAwake = false; doeant work like that, just turn it off
+        delayFrames = delayFramesDefault;
+    }
+
+    private void FixedUpdate()
+    {
+        delayFrames--;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (delayFrames <= 0) { source.Play(); delayFrames = delayFramesDefault; };
+        
+    }
+}
