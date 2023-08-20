2a300d8f9d6447e88cb7a116be5bf3e0ea29c10c
dawson-lefebvre <112987023+dawson-lefebvre@users.noreply.github.com>
2023-08-20 16:07:41 -0600

    Menu now works with GamePad

--- Assets/UI/UI Elements/MainMenu/MainMenuButtonBehaviours.cs
+++ Assets/UI/UI Elements/MainMenu/MainMenuButtonBehaviours.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.SceneManagement;
 
 public class MainMenuButtonBehaviours : MonoBehaviour
@@ -8,6 +9,7 @@ public class MainMenuButtonBehaviours : MonoBehaviour
     [SerializeField] GameObject mainMenuUI;
     [SerializeField] GameObject howToPlayUI;
     [SerializeField] GameObject creditsUI;
+    [SerializeField] EventSystem eventSystem;
     public void Quit()
     {
         Application.Quit();
@@ -19,22 +21,28 @@ public class MainMenuButtonBehaviours : MonoBehaviour
         SceneManager.LoadScene(1);
     }
 
+    [SerializeField] GameObject HTPBackButton;
     public void HowToPlay()
     {
         mainMenuUI.SetActive(false);
         howToPlayUI.SetActive(true);
+        eventSystem.SetSelectedGameObject(HTPBackButton);
     }
 
+    [SerializeField] GameObject CreditsBackButton;
     public void Credits()
     {
         mainMenuUI.SetActive(false);
         creditsUI.SetActive(true);
+        eventSystem.SetSelectedGameObject(CreditsBackButton);
     }
 
+    [SerializeField] GameObject MainMenuPlayButton;
     public void BackToMenu()
     {
         mainMenuUI.SetActive(true);
         howToPlayUI.SetActive(false);
         creditsUI.SetActive(false);
+        eventSystem.SetSelectedGameObject(MainMenuPlayButton);
     }
 }
