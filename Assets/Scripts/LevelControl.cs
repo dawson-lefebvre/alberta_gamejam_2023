f58e2f94578b0daab648be7c8a603864daf4f6c7
dawson-lefebvre <112987023+dawson-lefebvre@users.noreply.github.com>
2023-08-20 17:30:03 -0600

    game is built

--- Assets/Scripts/LevelControl.cs
+++ Assets/Scripts/LevelControl.cs
@@ -1,18 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class LevelControl : MonoBehaviour
 {
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
-        
+        Globals.cowsRemaining = 20; 
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(Globals.cowsRemaining == 0) 
+        {
+            SceneManager.LoadScene(0);
+        }
     }
 }
