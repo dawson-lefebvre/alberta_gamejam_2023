8f5632ad8005e0be28a4d7c98fd3888edd57dfaf
eHolmesSchool <squippysquerp@gmail.com>
2023-08-20 14:59:18 -0600

    Update CowBehaviour.cs

--- Assets/Scripts/CowBehaviour.cs
+++ Assets/Scripts/CowBehaviour.cs
@@ -7,14 +7,31 @@ public class CowBehaviour : MonoBehaviour
     Rigidbody2D rb;
     [SerializeField]GameObject pullPosition; //Position to tract towards when caught in beam
 
+    [SerializeField] AudioClip bonkClip;
+    [SerializeField] AudioClip collectionClip;
+    [SerializeField] AudioSource source;
+
+    [SerializeField] int FXDelayFramesDefault = 30;
+    int FXDelayFrames;
+
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+
+        FXDelayFrames = FXDelayFramesDefault;
     }
 
     public float pullForce = 5;
     public float maxSpeed = 5;
+
+
+    private void FixedUpdate()
+    {
+        FXDelayFrames--;
+    }
+
+
     private void OnTriggerStay2D(Collider2D collision) //Runs every frame that the cow is in a trigger
     {
         if(collision.tag == "TractorBeam") //Runs if the trigger is the tractor beam
@@ -40,12 +57,22 @@ public class CowBehaviour : MonoBehaviour
             Destroy(gameObject);
             Globals.cowsRescued++;
             Globals.cowsRemaining--;
+
+            if (FXDelayFrames <= 0) 
+            {
+                source.clip = bonkClip;
+                source.Play();
+                FXDelayFrames = FXDelayFramesDefault; 
+            };
         }
         else if(collision.tag == "KillBox") //Adds one to cows lost and subtracts one from cows remaining after destroying itself
         {
             Destroy(gameObject);
             Globals.cowsLost++;
             Globals.cowsRemaining--;
+
+            source.clip = bonkClip;
+            source.Play();
         }
     }
 }
